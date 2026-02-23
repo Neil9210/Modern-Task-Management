ae1928c0640f66168e43a866a2ce2424d1266831
Nitin Mathur <ddnitin2002@gmail.com>
2026-02-23 12:34:48 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
   build: {
     target: "esnext",
-    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes("node_modules")) {
-            const module = id.split("node_modules/")[1].split("/")[0];
-            return module;
+            return id.split("node_modules/")[1].split("/")[0];
           }
         },
       },
