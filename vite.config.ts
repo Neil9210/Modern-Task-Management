41fb3dbfcec320a4481d0b1bbd41a42cc9603c8a
Nitin Mathur <ddnitin2002@gmail.com>
2026-02-23 11:25:51 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: "/",   // important for Vercel
+
   plugins: [react()],
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./srs"), // because folder name = srs
     },
+  },
+
+  build: {
+    outDir: "dist",
   },
 });
