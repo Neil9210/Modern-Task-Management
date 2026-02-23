10135c4a0d693f565df01611c7074db4a26951e4
Nitin Mathur <ddnitin2002@gmail.com>
2026-02-23 12:37:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,4 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: "esnext",
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id.split("node_modules/")[1].split("/")[0];
-          }
-        },
-      },
-    },
-  },
 });
