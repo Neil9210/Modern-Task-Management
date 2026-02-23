d76d3f837afa64c56fc486b4b5a1153c9239dab1
Nitin Mathur <ddnitin2002@gmail.com>
2026-02-23 11:35:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
 
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./srs"), // because folder name = srs
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 
